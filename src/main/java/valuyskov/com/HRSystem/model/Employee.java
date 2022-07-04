85a3ba76129ecc2dd983e0e11c8a303ab20e4cad
LerValisk <aleksvaluyskov@gmail.com>
2022-07-04 18:33:20 +0200

    validated tomcat servlet and response from sql

--- src/main/java/valuyskov/com/HRSystem/model/Employee.java
+++ src/main/java/valuyskov/com/HRSystem/model/Employee.java
@@ -1,4 +1,4 @@
-package model;
+package valuyskov.com.HRSystem.model;
 
 
 import lombok.Getter;
@@ -17,7 +17,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="firt_name")
+    @Column(name="first_name")
     private String firstName;
     @Column(name="last_name")
     private String lastName;
