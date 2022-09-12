881535c81be62179367b10c2d0bac3fe5de07d80
LerValisk <aleksvaluyskov@gmail.com>
2022-09-12 20:36:10 +0200

    added find by email, EntityExist acception, updateByEmail/byId

--- src/main/java/valuyskov/com/HRSystem/service/EmployeeService.java
+++ src/main/java/valuyskov/com/HRSystem/service/EmployeeService.java
@@ -1,18 +1,18 @@
 package valuyskov.com.HRSystem.service;
 
-import valuyskov.com.HRSystem.exception.EmployeeAlreadyExistException;
 import valuyskov.com.HRSystem.exception.EntityNotFoundException;
 import valuyskov.com.HRSystem.model.Employee;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface EmployeeService {
 
-    Employee updateEmployee(Employee employee) throws EmployeeAlreadyExistException;
-    Employee createEmployee(Employee employee) throws EmployeeAlreadyExistException;
+    Employee updateEmployee ( Employee employee) ;
+    Employee updateEmployee ( Employee employee, boolean findByEmail) ;
+    Employee createEmployee(Employee employee) ;
     List getAllEmployees() throws EntityNotFoundException;
     Employee getEmployeeById (Long id) throws EntityNotFoundException;
     Long deleteEmployee(Long id) throws EntityNotFoundException;
+    Employee getEmployeeByEmail(String email) throws  EntityNotFoundException;
 
 }
